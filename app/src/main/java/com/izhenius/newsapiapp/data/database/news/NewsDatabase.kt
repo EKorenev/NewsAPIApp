b0489c40be4a9c7eeff9b37cd2c4d1b70eb4cedc
iZhenius <iZhenius@gmail.com>
2020-11-25 16:09:53 +0300

    Switched off database schema export

--- app/src/main/java/com/izhenius/newsapiapp/data/database/news/NewsDatabase.kt
+++ app/src/main/java/com/izhenius/newsapiapp/data/database/news/NewsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.izhenius.newsapiapp.data.database.news.dao.NewsArticleDao
 import com.izhenius.newsapiapp.data.database.news.model.DatabaseNewsArticle
 
-@Database(entities = [DatabaseNewsArticle::class], version = 1)
+@Database(entities = [DatabaseNewsArticle::class], version = 1, exportSchema = false)
 abstract class NewsDatabase : RoomDatabase() {
 
     abstract fun newsArticleDao(): NewsArticleDao
